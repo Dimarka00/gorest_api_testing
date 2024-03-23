f79b6c4bd06238fcb3c1896df09f7a39659807d2
Dima Klimko <dimadimarkinn@gmail.com>
2024-03-23 15:59:17 +0300

    Added utilities for JSON reading and 404 Not Found JSON

---
+++ utils/files_utils.py
@@ -0,0 +1,23 @@
+import json
+
+
+def get_test_data_path():
+    return "test_data"
+
+
+def get_common_response_path():
+    return f"{get_test_data_path()}/common/responses"
+
+
+def read_json_file_data(path):
+    with open(f"{path}.json", "r") as f:
+        data = json.load(f)
+    return data
+
+
+def read_json_test_data(request):
+    return read_json_file_data(f"{get_test_data_path()}/{request.node.originalname}")
+
+
+def read_json_common_response_data(file_name):
+    return read_json_file_data(f"{get_common_response_path()}/{file_name}")
