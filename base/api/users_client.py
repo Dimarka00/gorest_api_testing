a50005c702213452f13ef4d9b06e89b4a9d5f263
Dima Klimko <dimadimarkinn@gmail.com>
2024-03-23 15:51:46 +0300

    Added new methods to the Users Client and a method for attaching an API response

--- base/api/users_client.py
+++ base/api/users_client.py
@@ -4,13 +4,23 @@ from requests import Response
 from client.base_client import APIClient
 from client.bearer_auth import BearerAuth
 from models.users import DefaultUser, CreateUser, UpdateUser
+from utils.attach_api_response_to_test import attach_response
 from utils.constants.routes import APIRoutes
 
 
 class UsersClient(APIClient):
     @allure.step('Getting all users')
     def get_all_users_api(self) -> Response:
-        return self.client.request('GET', APIRoutes.USERS)
+        response = self.client.request('GET', APIRoutes.USERS)
+        attach_response(response.json())
+        return response
+
+    @allure.step('Getting user with id {user_id}')
+    def get_user_by_id_api(self, user_id: int):
+        response = self.client.request('GET', f'{APIRoutes.USERS}/{user_id}',
+                                       auth=BearerAuth())
+        attach_response(response.json())
+        return response
 
     @allure.step('Creating a new user')
     def post_user_api(self, payload: CreateUser):
@@ -26,10 +36,11 @@ class UsersClient(APIClient):
 
     @allure.step('Deleting user with id {user_id}')
     def delete_user_api(self, user_id: int):
-        return self.client.request('DELETE', f'{APIRoutes.USERS}/{user_id}')
+        return self.client.request('DELETE', f'{APIRoutes.USERS}/{user_id}',
+                                   auth=BearerAuth())
 
     def create_user(self) -> DefaultUser:
         payload = CreateUser()
         response = self.post_user_api(payload)
+        attach_response(response.json())
         return DefaultUser(**response.json())
-
